c43686097a70cce3b58b9ba7e4f96e744fb278d1
Stephen Paul Weber <singpolyma@singpolyma.net>
2012-11-13 17:02:17 -0500

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -1,7 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static char font[] = "Liberation Mono:pixelsize=12:antialias=false:autohint=false";
+static char font[] = "./LiberationMono-Regular.ttf:./LiberationMono-Bold.ttf";
+static int fontsize = 12;
 static int borderpx = 2;
 static char shell[] = "/bin/sh";
 
@@ -16,34 +17,33 @@ static unsigned int tabspaces = 8;
 #define WORD_BREAK " "
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
+SDL_Color colormap[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	{   0,   0,   0, 0 },//black
+	{ 128,   0,   0, 0 },//"red3",
+	{   0, 128,   0, 0 },//"green3",
+	{ 128, 128,   0, 0 },//"yellow3",
+	{   0,   0, 128, 0 },//"blue2",
+	{ 128,   0, 128, 0 },//"magenta3",
+	{   0, 128, 128, 0 },//"cyan3",
+	{ 192, 192, 192, 0 },//"gray90",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	{ 128, 128, 128, 0}, //"gray50",
+	{ 255,   0,   0, 0 },//red
+	{   0, 255,   0, 0 },//green
+	{ 255, 255,   0, 0 },//"yellow",
+	{   0,   0, 255, 0 },//"#0000ff",
+	{ 255,   0, 255, 0 },//"magenta",
+	{   0, 255, 255, 0 },//"cyan",
+	{ 255, 255, 255, 0 },//"white",
 
-	[255] = 0,
+	[255] = { 0, 0, 0, 0 },
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#333333",
+	{ 204, 204, 204, 0},
+	{  51,  51,  51, 0},
 };
-
 
 /*
  * Default colors (colorname index)
@@ -65,33 +65,36 @@ static unsigned int defaultucs = 257;
 
 /* key, mask, output */
 static Key key[] = {
-	{ XK_BackSpace, XK_NO_MOD, "\177" },
-	{ XK_Insert,    XK_NO_MOD, "\033[2~" },
-	{ XK_Delete,    XK_NO_MOD, "\033[3~" },
-	{ XK_Home,      XK_NO_MOD, "\033[1~" },
-	{ XK_End,       XK_NO_MOD, "\033[4~" },
-	{ XK_Prior,     XK_NO_MOD, "\033[5~" },
-	{ XK_Next,      XK_NO_MOD, "\033[6~" },
-	{ XK_F1,        XK_NO_MOD, "\033OP"   },
-	{ XK_F2,        XK_NO_MOD, "\033OQ"   },
-	{ XK_F3,        XK_NO_MOD, "\033OR"   },
-	{ XK_F4,        XK_NO_MOD, "\033OS"   },
-	{ XK_F5,        XK_NO_MOD, "\033[15~" },
-	{ XK_F6,        XK_NO_MOD, "\033[17~" },
-	{ XK_F7,        XK_NO_MOD, "\033[18~" },
-	{ XK_F8,        XK_NO_MOD, "\033[19~" },
-	{ XK_F9,        XK_NO_MOD, "\033[20~" },
-	{ XK_F10,       XK_NO_MOD, "\033[21~" },
-	{ XK_F11,       XK_NO_MOD, "\033[23~" },
-	{ XK_F12,       XK_NO_MOD, "\033[24~" },
+	{ SDLK_LEFT,      KMOD_ALT,  "\033[1;3D" },
+	{ SDLK_RIGHT,     KMOD_ALT,  "\033[1;3C" },
+
+	{ SDLK_BACKSPACE, 0, "\177" },
+	{ SDLK_INSERT,    0, "\033[2~" },
+	{ SDLK_DELETE,    0, "\033[3~" },
+	{ SDLK_HOME,      0, "\033[1~" },
+	{ SDLK_END,       0, "\033[4~" },
+	{ SDLK_PAGEUP,    0, "\033[5~" },
+	{ SDLK_PAGEDOWN,  0, "\033[6~" },
+	{ SDLK_F1,        0, "\033OP"   },
+	{ SDLK_F2,        0, "\033OQ"   },
+	{ SDLK_F3,        0, "\033OR"   },
+	{ SDLK_F4,        0, "\033OS"   },
+	{ SDLK_F5,        0, "\033[15~" },
+	{ SDLK_F6,        0, "\033[17~" },
+	{ SDLK_F7,        0, "\033[18~" },
+	{ SDLK_F8,        0, "\033[19~" },
+	{ SDLK_F9,        0, "\033[20~" },
+	{ SDLK_F10,       0, "\033[21~" },
+	{ SDLK_F11,       0, "\033[23~" },
+	{ SDLK_F12,       0, "\033[24~" },
 };
 
 /* Internal shortcuts. */
-#define MODKEY Mod1Mask
+#define MODKEY KMOD_ALT
 
 static Shortcut shortcuts[] = {
 	/* modifier		key		function	argument */
-	{ MODKEY|ShiftMask,	XK_Prior,	xzoom,		{.i = +1} },
-	{ MODKEY|ShiftMask,	XK_Next,	xzoom,		{.i = -1} },
+	{ MODKEY|KMOD_SHIFT,	SDLK_PAGEUP,	xzoom,		{.i = +1} },
+	{ MODKEY|KMOD_SHIFT,	SDLK_PAGEDOWN,	xzoom,		{.i = -1} },
 };
 
