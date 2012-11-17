c72d1ac33bf85594271192f052517e1ad436e096
Stephen Paul Weber <singpolyma@singpolyma.net>
2012-11-17 09:50:46 -0500

    build system does not need X11

--- config.mk
+++ config.mk
@@ -7,12 +7,9 @@ VERSION = 0.3
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
-
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC} -I/usr/include/freetype2
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lutil -lXext -lXft -lfontconfig
+INCS = -I. -I/usr/include
+LIBS = -L/usr/lib -lc -lutil
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\"
